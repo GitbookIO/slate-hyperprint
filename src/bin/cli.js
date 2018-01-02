c5ec78b5334fa54b8481e0f426da9dbeb864bc2e
Gabin <hello@gabinaureche.com>
2018-01-02 17:59:24 +0100

    Add CLI tool

---
+++ src/bin/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/* @flow */
+
+import fs from 'fs';
+import yaml from 'js-yaml';
+import Slate from 'slate';
+import meow from 'meow';
+import hyperprint from '../';
+
+const { input } = meow(`
+    Usage
+        $ slate-hyperprint <path>
+`);
+
+if (input.length > 0) {
+    const [path] = input;
+    const json = yaml.safeLoad(fs.readFileSync(path));
+    const document =
+        (json.state && json.state.document) || json.document || json;
+    const state = Slate.Value.create({ document });
+
+    console.log(hyperprint(state.document));
+}
